46dff792ad4ab61c083a518d25b0ca31bc0c32f9
Jacob Dudjak <134453268+OceanManCo@users.noreply.github.com>
2024-05-24 19:17:12 +0000

    Exercise 1.1, added all tests

--- main.cs
+++ main.cs
@@ -2,6 +2,38 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World! Its the best time to learn C#!");
+    personalInfo(0);
+    personalInfo(1);
+    personalInfo(2);
+    personalInfo(3);
   }
-}
+  public static void personalInfo(int test){//program that will run to print information
+ if(test==0){//these tests will be done for each of the required tests and called using the test variable
+   Console.WriteLine("Lucinda Potter");
+   Console.WriteLine("6/24/1992");
+   Console.WriteLine("work 000-000-0101");
+   Console.WriteLine("cell 000-000-0189");
+   }
+   else if(test==1){
+   Console.WriteLine("John Smith");
+   Console.WriteLine("8/15/1978");
+   Console.WriteLine("work 000-111-0101");
+   Console.WriteLine("cell 000-111-0189");
+   ;
+   }
+    else if(test==2){
+  Console.WriteLine("Chris Johnson");
+   Console.WriteLine("12/01/1987");
+   Console.WriteLine("work 000-222-0101");
+   Console.WriteLine("cell 000-222-0189");
+   ;
+    }
+    else{
+   Console.WriteLine("Robert Hall");
+   Console.WriteLine("2/27/1949");
+   Console.WriteLine("work 000-444-0101");
+   Console.WriteLine("cell 000-444-0189");
+    }
+  }
+
+  }
