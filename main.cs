84515851fc631ac94250ff6cebd11bc059032e89
Jacob Dudjak <134453268+OceanManCo@users.noreply.github.com>
2024-05-25 19:06:16 +0000

    Exercise 1.2 all tests

--- main.cs
+++ main.cs
@@ -2,38 +2,29 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    personalInfo(0);
-    personalInfo(1);
-    personalInfo(2);
-    personalInfo(3);
+    lyrics();
   }
-  public static void personalInfo(int test){//program that will run to print information
- if(test==0){//these tests will be done for each of the required tests and called using the test variable
-   Console.WriteLine("Lucinda Potter");
-   Console.WriteLine("6/24/1992");
-   Console.WriteLine("work 000-000-0101");
-   Console.WriteLine("cell 000-000-0189");
-   }
-   else if(test==1){
-   Console.WriteLine("John Smith");
-   Console.WriteLine("8/15/1978");
-   Console.WriteLine("work 000-111-0101");
-   Console.WriteLine("cell 000-111-0189");
-   ;
-   }
-    else if(test==2){
-  Console.WriteLine("Chris Johnson");
-   Console.WriteLine("12/01/1987");
-   Console.WriteLine("work 000-222-0101");
-   Console.WriteLine("cell 000-222-0189");
-   ;
-    }
-    else{
-   Console.WriteLine("Robert Hall");
-   Console.WriteLine("2/27/1949");
-   Console.WriteLine("work 000-444-0101");
-   Console.WriteLine("cell 000-444-0189");
+  public static void lyrics(){//program that will run to print information
+   Console.WriteLine("Oh, well imagine /");
+   Console.WriteLine("As I'm pacing the pews in a church corridor /");
+   Console.WriteLine("And I can't help but to hear /");
+   Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+  
+   Console.WriteLine("Oh-oh-oh-oh-oh,");
+   Console.WriteLine("oh-oh-oh-oh,");
+   Console.WriteLine("oh-oh-oh /");
+   Console.WriteLine("Caught in a bad romance...");
+  
+  Console.WriteLine("Coming out of my cage");
+   Console.WriteLine("And I've been doing just fine");
+   Console.WriteLine("Gotta gotta be down");
+   Console.WriteLine("Because I want it all ");
+    
+   Console.WriteLine("Now, this is a story all about how");
+   Console.WriteLine("My life got flipped-turned upside down");
+   Console.WriteLine("And I'd like to take a minute");
+   Console.WriteLine("Just sit right there");
+   Console.WriteLine("I'll tell you how I became the prince of a town called Bel-Air");
     }
   }
 
-  }
