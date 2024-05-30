9e5bb366c571e4866528d56ebb1f3dddc6c85724
Jacob Dudjak <134453268+OceanManCo@users.noreply.github.com>
2024-05-30 18:27:03 +0000

    Assignment 2.2

--- main.cs
+++ main.cs
@@ -2,15 +2,14 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    inToCm();
-    inToCm();
-    inToCm();
-    inToCm();
+    costOfMove(10,600);
+    costOfMove(6.77,15);
+    costOfMove(56.25,325);
+    costOfMove(103,3000);
   }
-  public static void inToCm(){//program that will run to print information
-   double In=Convert.ToDouble(Console.ReadLine());
-    In=In*2.54;
-  Console.WriteLine(In); 
+  public static void costOfMove(double hours, double miles){//program that will run to print information
+  double cost=200+(hours*150)+(miles*2);
+  Console.WriteLine("$"+cost); 
     }
   }
 
