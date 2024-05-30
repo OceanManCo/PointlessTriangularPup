de3a20fb3bf31232ae9615c99e0741a340855fc0
Jacob Dudjak <134453268+OceanManCo@users.noreply.github.com>
2024-05-30 16:51:12 +0000

    2.1.1(updated to take user input)

--- main.cs
+++ main.cs
@@ -2,13 +2,14 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    inToCm(5);
-    inToCm(50);
-    inToCm(-15);
-    inToCm(15994);
+    inToCm();
+    inToCm();
+    inToCm();
+    inToCm();
   }
-  public static void inToCm(double In){//program that will run to print information
-   In=In*2.54;
+  public static void inToCm(){//program that will run to print information
+   double In=Convert.ToDouble(Console.ReadLine());
+    In=In*2.54;
   Console.WriteLine(In); 
     }
   }
