363c961da9a449d799bc96e07c0487a365f9844a
Jacob Dudjak <134453268+OceanManCo@users.noreply.github.com>
2024-06-15 07:03:27 +0000

    4.2

--- main.cs
+++ main.cs
@@ -1,19 +1,50 @@
 using System;
-
+using System.Linq;
 class Program {
   public static void Main (string[] args) {
-  Perfect(496);
-  Perfect(68);
-  Perfect(5112);
-  Perfect(8128);
+     HomeSales(0,0,0,0);
     }
-  public static void Perfect (double number){// this will take all numbers from 1-1000 that are perfect
-    if(number== 6||number== 28||number== 496||number== 8128){
-      Console.WriteLine("Perfect Number");
+  public static void HomeSales (int grandtot,int dsale,int esale, int fsale){
+    Console.WriteLine("Hello sales repesentive of Holiday Homes! please input your username! Or to end this service type Z");
+    
+    string user=Console.ReadLine();
+    if(user=="D"||user=="d"){
+      Console.WriteLine("Hello Danielle! Please input the sale you just made");
+      dsale=Int32.Parse(Console.ReadLine());//this will parse the text and make it a number
+      grandtot+=dsale;
+      HomeSales(grandtot,dsale,esale,fsale);
+      }
+    else if(user=="E"||user=="e"){
+      Console.WriteLine("Hello Edward! Please input the sale you just made");
+      esale=Int32.Parse(Console.ReadLine());//this will parse the text and make it a number
+      grandtot+=esale;
+      HomeSales(grandtot,dsale,esale,fsale);
+      }
+    else if(user=="F"||user=="f"){
+      Console.WriteLine("Hello Francis! Please input the sale you just made");
+      fsale=Int32.Parse(Console.ReadLine());//this will parse the text and make it a number
+      grandtot+=fsale;
+      HomeSales(grandtot,dsale,esale,fsale);
+      }
+    else if(user=="Z"||user=="z"){
+      int[] datlist={dsale,esale,fsale};
+      if(datlist.Max()==dsale){
+        Console.WriteLine("Highest Sale: D");
+      }
+      else if(datlist.Max()==esale){
+        Console.WriteLine("Highest Sale: E");
+      }
+      else{
+        Console.WriteLine("Highest Sale: F");
+      }
+      Console.WriteLine("Grand Total: $"+grandtot);
+      }
+    else{
+      Console.WriteLine("that is not a valid responce please try again");
+      HomeSales(grandtot,dsale,esale,fsale);
     }
-    else{//this shows the result if not the four perfect numebrs
-      Console.WriteLine("Not A Perfect Number");
-    }
+  
+    
     }
   }
 
