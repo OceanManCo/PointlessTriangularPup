91348f3970e65cdc31db54df51bbb0170750ccac
Jacob Dudjak <134453268+OceanManCo@users.noreply.github.com>
2024-06-16 19:07:39 +0000

    4.2(commented)

--- main.cs
+++ main.cs
@@ -8,27 +8,27 @@ class Program {
     Console.WriteLine("Hello sales repesentive of Holiday Homes! please input your username! Or to end this service type Z");
     
     string user=Console.ReadLine();
-    if(user=="D"||user=="d"){
+    if(user=="D"||user=="d"){//if statement to find what salesperson is running the program
       Console.WriteLine("Hello Danielle! Please input the sale you just made");
       dsale=Int32.Parse(Console.ReadLine());//this will parse the text and make it a number
-      grandtot+=dsale;
-      HomeSales(grandtot,dsale,esale,fsale);
+      grandtot+=dsale;//adds the sale to that salespersons total
+      HomeSales(grandtot,dsale,esale,fsale);//this will use recursively call the method
       }
     else if(user=="E"||user=="e"){
       Console.WriteLine("Hello Edward! Please input the sale you just made");
       esale=Int32.Parse(Console.ReadLine());//this will parse the text and make it a number
-      grandtot+=esale;
-      HomeSales(grandtot,dsale,esale,fsale);
+      grandtot+=esale;//adds the sale to that salespersons total
+      HomeSales(grandtot,dsale,esale,fsale);//this will use recursively call the method
       }
     else if(user=="F"||user=="f"){
       Console.WriteLine("Hello Francis! Please input the sale you just made");
       fsale=Int32.Parse(Console.ReadLine());//this will parse the text and make it a number
-      grandtot+=fsale;
-      HomeSales(grandtot,dsale,esale,fsale);
+      grandtot+=fsale;//adds the sale to that salespersons total
+      HomeSales(grandtot,dsale,esale,fsale);//this will use recursively call the method
       }
     else if(user=="Z"||user=="z"){
       int[] datlist={dsale,esale,fsale};
-      if(datlist.Max()==dsale){
+      if(datlist.Max()==dsale){//this will find the greatest sale
         Console.WriteLine("Highest Sale: D");
       }
       else if(datlist.Max()==esale){
@@ -39,9 +39,9 @@ class Program {
       }
       Console.WriteLine("Grand Total: $"+grandtot);
       }
-    else{
-      Console.WriteLine("that is not a valid responce please try again");
-      HomeSales(grandtot,dsale,esale,fsale);
+    else{//handles exceptions
+      Console.WriteLine("that is not a valid response please try again");
+      HomeSales(grandtot,dsale,esale,fsale);//this will use recursively call the method
     }
   
     
