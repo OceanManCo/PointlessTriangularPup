434422e7f92aefc29ca18c5982ca094462e39b8d
Jacob Dudjak <134453268+OceanManCo@users.noreply.github.com>
2024-07-01 03:54:41 +0000

    project 0-2 fixed

--- GreenvilleIdol.cs
+++ GreenvilleIdol.cs
@@ -14,7 +14,7 @@ public static void OnRun(){
         int lastYear=Int32.Parse(Console.ReadLine());//reads old contestants
         Console.WriteLine("How many contestants this year?");
         int thisYear=Int32.Parse(Console.ReadLine());//reads new contestants
-        Console.WriteLine("Last year's competition had "+lastYear+ "contestants, and this year's has "+thisYear +" contestants \n Revenue expected this year is $"+thisYear*25 +" ");//calcuates revenue and shows difference
+        Console.WriteLine("Last year's competition had "+lastYear+" contestants, and this year's has "+thisYear +" contestants \n Revenue expected this year is $"+thisYear*25 +" ");//calcuates revenue and shows difference
         if(thisYear>lastYear){
             yearsize=true;
             if(lastYear*2-thisYear>0){//checks if size is bigger or lower than half
@@ -23,10 +23,11 @@ public static void OnRun(){
             else if(lastYear*2-thisYear<=0){//checks to see if its bigger than half
                 Console.WriteLine("the competiton is twice as big!");
             }
+
+            }
             else{
                 Console.WriteLine("A tighter race this year! Come out and cast your vote!");
-            }
-            }
+        }
             Console.WriteLine("It is "+yearsize +" that this year's competition is bigger than last year's.");
         
         }
